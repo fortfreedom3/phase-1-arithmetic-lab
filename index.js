19a2007981e94d5014b0770b6e78e839c151ad78
Morgan <morganlbyrne@gmail.com>
2021-11-01 22:35:24 -0400

    commit tree 6bef05c67b7fadb64a1a9b261a58dc098c1f7ed7

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2; 
+const multiply = num1 * num2;
+
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+
+const num3 = 24;
+const num4 = 5
+const mod = num3 % num4;
+
+
+const set1 = [1, 2, 3, 4, 20];
+const max = Math.max(...set1);
+
+
+
+
+
+
